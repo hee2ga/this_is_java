add4c2f175b63739fe18a0fc5867e1bb9ce620b2
heega <jsa7868@gmail.com>
2025-06-11 20:32:57 +0900

    ch02

---
+++ chap02/src/ch02/sec01/VariableInitializationExample.java
@@ -0,0 +1,11 @@
+package ch02.sec01;
+
+public class VariableInitializationExample {
+	public static void main(String[] args) {
+		int value;
+		// value 변수가 초기화 되지않아 오류 발생
+		int reault=value+10;
+		
+		System.out.println(reault);
+	}
+}
