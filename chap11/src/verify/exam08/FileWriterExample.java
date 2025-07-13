6c8155a471849f925b89e9db39d65c8b715468a1
heega <jsa7868@gmail.com>
2025-07-14 00:01:21 +0900

    ch11

---
+++ chap11/src/verify/exam08/FileWriterExample.java
@@ -0,0 +1,14 @@
+package verify.exam08;
+
+import java.io.IOException;
+
+public class FileWriterExample {
+	public static void main(String[] args) {
+		
+		try(FileWriter fw=new FileWriter("file.txt")) {
+			fw.write("Java");
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
